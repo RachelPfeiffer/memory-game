10ac301972104ad744e01c13d3e585e1d0430a71
Rachel Pfeiffer <rochelfried9@gmail.com>
2018-10-15 09:38:16 +0300

    final formatting changes

--- js/app.js
+++ js/app.js
@@ -6,16 +6,16 @@ function init() {
   board.classList.add('board');
   page.appendChild(board);
   //b. For each icon, make a card. Show cards closed.
-  const icons = ["fa-bomb","fa-bomb", "fa-trophy",
-  "fa-trophy", "fa-phone", "fa-phone",
-  "fa-camera-retro", "fa-camera-retro", "fa-cloud",
-  "fa-cloud", "fa-truck", "fa-truck", "fa-beer",
-  "fa-beer", "fa-spinner", "fa-spinner"];
+  const icons = ['fa-bomb','fa-bomb', 'fa-trophy',
+  'fa-trophy', 'fa-phone', 'fa-phone',
+  'fa-camera-retro', 'fa-camera-retro', 'fa-cloud',
+  'fa-cloud', 'fa-truck', 'fa-truck', 'fa-beer',
+  'fa-beer', 'fa-spinner', 'fa-spinner'];
 
   for (icon of icons) {
     const newCard = document.createElement('div');
-    newCard.classList.add("card", "fa", icon,
-    "closed", "in-game");
+    newCard.classList.add('card', 'fa', icon,
+    'closed', 'in-game');
     board.appendChild(newCard);
   };
 
@@ -30,10 +30,10 @@ function init() {
   shuffle();
 
   //d. Add 3 stars to the star-list.
-  const starList = document.querySelector(".starlist");
+  const starList = document.querySelector('.starlist');
   function createStars() {
     const newStar = document.createElement('li');
-    newStar.classList.add("fa", "fa-star");
+    newStar.classList.add('fa', 'fa-star');
     starList.appendChild(newStar);
   };
   createStars();
@@ -62,7 +62,7 @@ matchingFunctionality(e) {
   //faster perf
   if (e.target.classList.contains('button')) {
     refresh();
-  } else if (e.target.classList.contains("in-game")) {
+  } else if (e.target.classList.contains('in-game')) {
   //b. If it's the first click:
     if (isGameRunning === false) {
       //  i. start the timer.
@@ -71,34 +71,34 @@ matchingFunctionality(e) {
       function countSeconds() {
         seconds+=1;
         function displayTime() {
-          timer = document.querySelector(".timer");
-          let formattedTime = ("0" + (Math.floor(seconds/60)).toString()).slice(-2) + ":" + ("0" + (seconds % 60).toString()).slice(-2);
+          timer = document.querySelector('.timer');
+          let formattedTime = ('0' + (Math.floor(seconds/60)).toString()).slice(-2) + ':' + ('0' + (seconds % 60).toString()).slice(-2);
           timer.innerText = formattedTime;
         };
         displayTime();
       };
       //  ii. start counting the moves.
-      mover = document.querySelector(".moves");
+      mover = document.querySelector('.moves');
       mover.innerText = moves;
-      //  iii. flip the "game in progress" switch.
+      //  iii. flip the game in progress switch.
       isGameRunning = true;
     };
   //c. If 2 cards are already open, do not allow to open any more cards.
-    const openCards = document.querySelectorAll(".open");
+    const openCards = document.querySelectorAll('.open');
     if (openCards.length === 2) {
       null;
     } else {
     //d. otherwise, check if the card is already open.
-    if (e.target.classList.contains("open")) {
+    if (e.target.classList.contains('open')) {
       //i. if it's open, this is the second click on the same card and it needs to be closed and removed from picked array.
-      e.target.classList.add("closed");
-      e.target.classList.remove("open");
+      e.target.classList.add('closed');
+      e.target.classList.remove('open');
       openCardIndex = picked.indexOf(e.target.classList.value);
       picked.splice(openCardIndex, 1);
     } else {
       //ii. if it's closed, open the card and add to picked array.
-      e.target.classList.add("open");
-      e.target.classList.remove("closed");
+      e.target.classList.add('open');
+      e.target.classList.remove('closed');
       picked.push(e.target.classList.value);
     }
   };
@@ -106,9 +106,9 @@ matchingFunctionality(e) {
   function checkMatch() {
     if (picked[0] === picked[1]) {
       const onMatch = function() {
-        const clickedCards = document.querySelectorAll(".open");
+        const clickedCards = document.querySelectorAll('.open');
         for (card of clickedCards) {
-          card.classList = ("card match");
+          card.classList = ('card match');
           console.log(card);
         }
         matches += 1;
@@ -118,9 +118,9 @@ matchingFunctionality(e) {
       updateStars();
     } else {
       const notMatch = function() {
-        const clickedCards = document.querySelectorAll(".open");
+        const clickedCards = document.querySelectorAll('.open');
         for (card of clickedCards) {
-          card.classList.add("closed");
+          card.classList.add('closed');
           card.classList.remove('open');
         }
       };
@@ -162,20 +162,20 @@ const isGameOver = function isGameOver() {
 };
 //a. run modal - win for win and lose for lose. Ask player if they want to play again.
   function youWin() {
-    const winBox = document.createElement("div");
-    winBox.classList.add("win");
-    winBox.innerHTML = "<div><header>You win!</header><p>YOUR STATS <br><br> Moves: " + mover.innerText + " <br> Star Rating: " + starCounter + "<br> Time: " + timer.innerText + "</p><button class=\"button\">Play<br>again?</button></div>";
+    const winBox = document.createElement('div');
+    winBox.classList.add('win');
+    winBox.innerHTML = '<div><header>You win!</header><p>YOUR STATS <br><br> Moves: ' + mover.innerText + ' <br> Star Rating: ' + starCounter + '<br> Time: ' + timer.innerText + '</p><button class="button">Play<br>again?</button></div>';
     board.appendChild(winBox);
   };
 
   function youLose() {
-    const loseBox = document.createElement("div");
-    loseBox.classList.add("win");
-    loseBox.innerHTML = "<span>You lose.</span><br><button class=\"button\">Play<br>again?</button>";
+    const loseBox = document.createElement('div');
+    loseBox.classList.add('win');
+    loseBox.innerHTML = '<span>You lose.</span><br><button class="button">Play<br>again?</button>';
     board.appendChild(loseBox);
     let cards = document.querySelectorAll('.card');
     for (card of cards) {
-          card.classList = ("card closed");
+          card.classList = ('card closed');
     }
   }
 
@@ -185,10 +185,10 @@ function resetGame() {
   picked = [];
   moves = 0;
   mover = document.querySelector('.moves');
-  mover.innerText = "";
+  mover.innerText = '';
   seconds = 0;
   timer = document.querySelector('.timer');
-  timer.innerText = "00:00";
+  timer.innerText = '00:00';
   matches = 0;
   board.parentElement.removeChild(board);
   starCounter = 0;
